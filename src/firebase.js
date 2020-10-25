bdba7411d984c4b6a980881e7266f0f97898d665
Premti <premti@gmail.com>
2020-10-25 16:19:36 +0000

    firebase linked to app

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCdfzpAH_GcGvoLfvbhX4GH1szqsXaoXwk",
+    authDomain: "social-media-app-c1721.firebaseapp.com",
+    databaseURL: "https://social-media-app-c1721.firebaseio.com",
+    projectId: "social-media-app-c1721",
+    storageBucket: "social-media-app-c1721.appspot.com",
+    messagingSenderId: "206209250684",
+    appId: "1:206209250684:web:5fb2a76fc6917e69a0a0f9",
+    measurementId: "G-6Q3GQCF81K"
+  };

9b3c71fdc022d502154437659fc0bdaa6c35eb0d
Premti <premti@gmail.com>
2020-11-01 21:33:25 +0000

    styling made to make story look like a card

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -6,7 +6,8 @@ function StoryReel(){
     return (
         <div className="storyReel">
             <Story 
-            
+            image="https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/1200px-Manchester_United_FC_crest.svg.png"
+            profileSrc="https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/18157874_10210897124057026_8516573728742171075_n.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=MK6kU8UejbMAX-E2XfZ&_nc_ht=scontent-lht6-1.xx&oh=b6d21f54b364b065675f0c979fe3a2c5&oe=5FBC196C"
             title="Premti Krasniqi"/>
         </div>
     )

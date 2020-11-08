39c1198225086453c2388d0ba627ad9d76188a87
Premti <premti@gmail.com>
2020-11-08 12:06:55 +0000

    post component created, dummy props being used to test

--- src/Feed.js
+++ src/Feed.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './Feed.css'
 import StoryReel from './StoryReel'
 import MessageSender from './MessageSender'
+import Post from './Post'
 
 function Feed(){
     return <div className="feed">
         <StoryReel />
         <MessageSender />
+
+        <Post 
+        profilePic="https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/18157874_10210897124057026_8516573728742171075_n.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=MK6kU8UejbMAX-E2XfZ&_nc_ht=scontent-lht6-1.xx&oh=b6d21f54b364b065675f0c979fe3a2c5&oe=5FBC196C"
+        message="working?"
+        timestamp="this is a timestamp"
+        usename="Premti"
+        image="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg"
+        />
     </div>
 }
 
--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './MessageSender.css'
 import { Avatar } from "@material-ui/core"
 import VideoCamIcon from "@material-ui/icons/Videocam"
@@ -8,19 +8,31 @@ import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon"
 
 function MessageSender() {
 
+    const [input, setInput] = useState('')
+    const [imageUrl, setImageUrl] = useState('')
+
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        setInput("")
+        setImageUrl("")
     }
     return(
         <div className="messageSender">
             <div className="messageSender__top">
                 <Avatar />
                 <form>
-                    <input type="text"
-                     placeholder="What's on your mind"
+                    <input
+                    value={input}
+                    onChange={e => setInput(e.target.value)}
+                     type="text"
+                     placeholder="What's on your mind?"
                      className="messageSender__input"/>
 
-                    <input type="text" placeholder="image URL (optional)"
+                    <input 
+                    value={imageUrl}
+                    onChange={e => setImageUrl(e.target.value)}
+                    type="text" placeholder="image URL (optional)"
                     />
                     <button onClick={handleSubmit} type="submit">
                         Hidden submit

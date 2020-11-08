266a467f9562d8950e4d7739eb0c49583efe7c59
Premti <premti@gmail.com>
2020-11-08 12:24:27 +0000

    added material ui icons for posts, also did all styling for a post

--- src/Feed.js
+++ src/Feed.js
@@ -16,6 +16,13 @@ function Feed(){
         usename="Premti"
         image="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg"
         />
+        <Post 
+        profilePic="https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/18157874_10210897124057026_8516573728742171075_n.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=MK6kU8UejbMAX-E2XfZ&_nc_ht=scontent-lht6-1.xx&oh=b6d21f54b364b065675f0c979fe3a2c5&oe=5FBC196C"
+        message="working?"
+        timestamp="this is a timestamp"
+        usename="Premti"
+        image="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg"
+        />
     </div>
 }
 
